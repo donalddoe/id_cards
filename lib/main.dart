33b23e893167bf9218a692d95b63f36b937d4468
Donalddoe <Donald.Seanedzu@azubiafrica.org>
2020-10-18 17:32:47 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: DonaldCard()
+));
+
+class DonaldCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: Text('Donald ID Card'),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
+        elevation: 0.0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: 
+            CircleAvatar(
+              backgroundImage: AssetImage('assets/girl.jpg'),
+              radius: 40.0,
+            ),
+            ),
+            Divider(
+              height: 60.0,
+              color: Colors.grey[800],
+            ),
+            Text(
+              'NAME',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 10.0,),
+             Text(
+              'Farida',
+              style: TextStyle(
+                color: Colors.amber[200],
+                letterSpacing: 2.0,
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+            SizedBox(height: 30.0,),
+            Text(
+              'CURRENT LEVEL',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 10.0,),
+             Text(
+              '300',
+              style: TextStyle(
+                color: Colors.amber[200],
+                letterSpacing: 2.0,
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+            SizedBox(height: 30.0,),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(width: 10.0,),
+                Text(
+                  'farida.beacher@azubiafrica.com',
+                  style: TextStyle(
+                  color: Colors.grey[400],
+                   fontSize: 18.0,
+                   letterSpacing: 1.0, 
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
