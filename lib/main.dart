7e15b5103d117a6cc686507f62f4588c28210730
Donalddoe <Donald.Seanedzu@azubiafrica.org>
2020-10-18 22:35:45 +0000

    added an increment button

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: DonaldCard()
-));
+void main() => runApp(MaterialApp(home: DonaldCard()));
 
-class DonaldCard extends StatelessWidget {
+class DonaldCard extends StatefulWidget {
+  @override
+  _DonaldCardState createState() => _DonaldCardState();
+}
+
+class _DonaldCardState extends State<DonaldCard> {
+  int donaldlevel = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,17 +20,25 @@ class DonaldCard extends StatelessWidget {
         backgroundColor: Colors.grey[850],
         elevation: 0.0,
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            donaldlevel += 100;
+          });
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.grey,
+      ),
       body: Padding(
         padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Center(
-              child: 
-            CircleAvatar(
-              backgroundImage: AssetImage('assets/girl.jpg'),
-              radius: 40.0,
-            ),
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/girl.jpg'),
+                radius: 40.0,
+              ),
             ),
             Divider(
               height: 60.0,
@@ -38,17 +51,20 @@ class DonaldCard extends StatelessWidget {
                 letterSpacing: 2.0,
               ),
             ),
-            SizedBox(height: 10.0,),
-             Text(
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
               'Farida',
               style: TextStyle(
-                color: Colors.amber[200],
-                letterSpacing: 2.0,
-                fontSize: 30.0,
-                fontWeight: FontWeight.bold
-              ),
+                  color: Colors.amber[200],
+                  letterSpacing: 2.0,
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold),
             ),
-            SizedBox(height: 30.0,),
+            SizedBox(
+              height: 30.0,
+            ),
             Text(
               'CURRENT LEVEL',
               style: TextStyle(
@@ -56,30 +72,35 @@ class DonaldCard extends StatelessWidget {
                 letterSpacing: 2.0,
               ),
             ),
-            SizedBox(height: 10.0,),
-             Text(
-              '300',
-              style: TextStyle(
-                color: Colors.amber[200],
-                letterSpacing: 2.0,
-                fontSize: 30.0,
-                fontWeight: FontWeight.bold
-              ),
+            SizedBox(
+              height: 10.0,
             ),
-            SizedBox(height: 30.0,),
+            Text(
+              '$donaldlevel',
+              style: TextStyle(
+                  color: Colors.amber[200],
+                  letterSpacing: 2.0,
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
             Row(
               children: <Widget>[
                 Icon(
                   Icons.email,
                   color: Colors.grey[400],
                 ),
-                SizedBox(width: 10.0,),
+                SizedBox(
+                  width: 10.0,
+                ),
                 Text(
                   'farida.beacher@azubiafrica.com',
                   style: TextStyle(
-                  color: Colors.grey[400],
-                   fontSize: 18.0,
-                   letterSpacing: 1.0, 
+                    color: Colors.grey[400],
+                    fontSize: 18.0,
+                    letterSpacing: 1.0,
                   ),
                 ),
               ],
